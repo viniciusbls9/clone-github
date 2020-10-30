09c6db2dd424c81f6f59baa08fc3b7157c4290da
Vinicius Benedito <beneditoloura@gmail.com>
2020-10-30 20:35:28 -0300

    Aplicação sendo povoada através da API do github

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 import { RiBookMarkLine } from 'react-icons/ri';
-
-
 import ProfileData from '../../components/ProfileData';
 import RepoCard from '../../components/RepoCard';
 import RandomCalendar from '../../components/RandomCalendar';
 
 import './styles.css';
+import { APIUser, APIRepo } from '../../@types';
 
-const pages: React.FC = () => {
+interface Data {
+	user?: APIUser;
+	repos?: APIRepo[];
+	error?: string;
+}
+
+const Profile: React.FC = () => {
+	const { username = 'viniciusbls9' } = useParams();
+	const [data, setData] = useState<Data>();
+
+	useEffect(() => {
+		Promise.all([
+			fetch(`https://api.github.com/users/${username}`),
+			fetch(`https://api.github.com/users/${username}/repos`),
+		]).then(async (responses) => {
+			const [userResponse, reposResponse] = responses;
+
+			if (userResponse.status === 404) {
+				setData({ error: 'User not found!' });
+				return;
+			}
+			const user = await userResponse.json();
+			const repos = await reposResponse.json();
+
+			const shuffledRepos = repos.sort(() => 0.50 - Math.random());
+			const slicedRepos = shuffledRepos.slice(0, 6);
+
+			setData({
+				user,
+				repos: slicedRepos,
+			});
+
+		})
+	}, [username]);
+
+	if (data?.error) {
+		return <h1>{data.error}</h1>
+	}
+
+	if(!data?.user || !data?.repos) {
+		return <h1>Loading....</h1>
+	}
 
 	const TabContent = () => (
 		<div className="content">
 			<RiBookMarkLine />
 			<span className="label">Repositories</span>
-			<span className="number">26</span>
+			<span className="number">{data.user?.public_repos}</span>
 		</div>
 	)
 
@@ -32,15 +74,15 @@ const pages: React.FC = () => {
 			<div className="main">
 				<div className="left-side">
 					<ProfileData
-						username={'viniciusbls9'}
-						name={'VInicius Benedito'}
-						avatarUrl={'https://avatars1.githubusercontent.com/u/40433674?v=4'}
-						followers={887}
-						following={7}
-						company={'Rocketseat'}
-						location={'São Paulo, Brazil'}
-						email={'beneditoloura@gmail.com'}
-						blog={undefined}
+						username={data.user.login}
+						name={data.user.name}
+						avatarUrl={data.user.avatar_url}
+						followers={data.user.followers}
+						following={data.user.following}
+						company={data.user.company}
+						location={data.user.location}
+						email={data.user.email}
+						blog={data.user.blog}
 					/>
 				</div>
 
@@ -53,15 +95,15 @@ const pages: React.FC = () => {
 						<h2>Random repos</h2>
 
 						<div>
-							{[1, 2, 3, 4, 5, 6].map(n => (
+							{data.repos.map((item) => (
 								<RepoCard
-									key={n}
-									username={'viniciusbls9'}
-									reponame={'whatsappclone'}
-									description={'whatsappclone'}
-									language={n % 3 === 0 ? 'Javascript' : 'Typescript'}
-									stars={8}
-									forks={4}
+									key={item.name}
+									username={item.owner.login}
+									reponame={item.name}
+									description={item.description}
+									language={item.language}
+									stars={item.stargazers_count}
+									forks={item.forks}
 								/>
 							))}
 						</div>
@@ -77,4 +119,4 @@ const pages: React.FC = () => {
 	);
 }
 
-export default pages;
+export default Profile;
