45b8ee80b3cde3db0a581e6486ab959f5335977b
Vinicius Benedito <beneditoloura@gmail.com>
2020-10-30 20:40:51 -0300

    Componente Header finalizado com a API

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaGithub } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+
+
 
 import './styles.css';
 
 const Header: React.FC = () => {
+    const [search, setSearch] = useState('');
+    const navigate = useNavigate();
+
+    function handleSubmit(event: React.FormEvent) {
+        event.preventDefault();
+
+        navigate(`/${search.toLowerCase().trim()}`);
+    }
+
     return (
         <div className="container">
             <FaGithub className="github-logo" />
 
-            <form className="search-form">
-                <input type="text" placeholder="Enter Username or repo..." />
+            <form className="search-form" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Enter Username or repo..."
+                    value={search}
+                    onChange={e => setSearch(e.currentTarget.value)}
+                />
             </form>
         </div>
     );
